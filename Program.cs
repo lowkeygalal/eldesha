4c45b8b1cece4a4b3997ad2adf85257225e9bed4
lowkeygalal <Ahmed.GalalIS21@fcai.usc.edu.eg>
2025-06-25 21:36:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,229 @@
+﻿namespace assign_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //I'm sorry I'm late but I have an excuse and i want a feedback please it's just 50 minutes lat :(
+            #region (q1)Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.WriteLine("Enter your number");
+            //int.TryParse(Console.ReadLine(),out int Num);
+            //if (Num % 3 == 0 && Num % 4 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+            #endregion
+
+            #region (q2) Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive
+            //Console.WriteLine("Enter your number");
+            //int.TryParse(Console.ReadLine(), out int Num);
+            //if(Num>0)
+            //{
+            //    Console.WriteLine("positive");
+            //}
+            //if (Num < 0)
+            //{
+            //    Console.WriteLine("negative");
+            //}
+
+            #endregion
+
+            #region (q3) Write a program that takes 3 integers from the user then prints the max element and the min element
+            //Console.WriteLine("Enter 3 numbers");
+            //int.TryParse(Console.ReadLine(), out int Num1);
+            //int.TryParse(Console.ReadLine(), out int Num2);
+            //int.TryParse(Console.ReadLine(), out int Num3);
+            //Console.Clear();
+            //if(Num1>Num2&&Num1>Num3)
+            //{
+            //    Console.WriteLine($"max element = {Num1}");
+            // if(Num2<Num3)
+            //    {
+            //        Console.WriteLine($"min element = {Num2}");
+            //    }
+            // else
+            //    {
+            //        Console.WriteLine($"min element = {Num3}");
+            //    }
+
+            //}
+
+            //else if (Num2 > Num1 && Num2 > Num3)
+            //{
+            //    Console.WriteLine($"max element = {Num2}");
+            //    if (Num1 < Num3)
+            //    {
+            //        Console.WriteLine($"min element = {Num1}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"min element = {Num3}");
+            //    }
+
+            //}
+
+            //else if (Num3 > Num1 && Num3 > Num2)
+            //{
+            //    Console.WriteLine($"max element = {Num3}");
+            //    if (Num1 < Num2)
+            //    {
+            //        Console.WriteLine($"min element = {Num1}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"min element = {Num2}");
+            //    }
+
+            //}
+
+            #endregion
+
+            #region (q4) Write a program that allows the user to insert an integer number then check If a number is even or odd
+            //Console.WriteLine("Enter a number");
+            //int.TryParse(Console.ReadLine(), out int Num);
+            //if(Num%2==0)
+            //{
+            //    Console.WriteLine("The Number is Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Number is Odd");
+            //}
+
+            #endregion
+
+            #region (q5) Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant)
+            //Console.WriteLine("Enter a Character");
+            //char.TryParse(Console.ReadLine(), out char C);
+            //if(C=='a'|| C == 'A'|| C == 'e'|| C == 'E'|| C == 'i'|| C == 'I' || C == 'o' || C == 'O' || C == 'u' || C == 'U')
+            //{
+            //    Console.WriteLine("Vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Consonant");
+            //}
+
+            #endregion
+
+            #region (q10) Write a program to enter marks of five subjects and calculate total, average and percentage
+
+            //Console.WriteLine("Enter your 5 subjects");
+            //int.TryParse(Console.ReadLine(), out int Sub1);
+            //int.TryParse(Console.ReadLine(), out int Sub2);
+            //int.TryParse(Console.ReadLine(), out int Sub3);
+            //int.TryParse(Console.ReadLine(), out int Sub4);
+            //int.TryParse(Console.ReadLine(), out int Sub5);
+            //double Perc = ((Sub1 + Sub2 + Sub3 + Sub4 + Sub5) / 500.0)*100;
+            //Console.Clear();
+            //Console.WriteLine($"Total marks = {(Sub1 + Sub2 + Sub3 + Sub4 + Sub5)}");
+            //Console.WriteLine($"Average Marks = {(Sub1+Sub2+Sub3+Sub4+Sub5)/5}");
+            //Console.WriteLine($"Percentage = {Perc}%");
+
+            #endregion
+
+            #region (q11) Write a program to input the month number and print the number of days in that month
+            //Console.WriteLine("Enter The Number of the month you want to know");
+            //int.TryParse(Console.ReadLine(), out int Num);
+            //switch(Num)
+            //{
+            //    case 1: Console.WriteLine("The Number of days in Jan is 31");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("The Number of days in Feb is 28 or 29");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("The Number of days in Mar is 31");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("The Number of days in Apr is 30");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("The Number of days in May is 31");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("The Number of days in Jun is 30");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("The Number of days in Jul is 31");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("The Number of days in Aug is 31");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("The Number of days in Sep is 30");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("The Number of days in Oct is 31");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("The Number of days in Nov is 30");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("The Number of days in Dec is 31");
+            //        break;
+            //    default: Console.WriteLine("This is not a month");
+            //        break;
+            #endregion
+
+            #region(q12) Write a program to create a Simple Calculator
+            //Console.WriteLine("What Operation do you want to do from these operations? ( + , - , * , / , % )");
+            //char.TryParse(Console.ReadLine(), out char Op);
+            //if(Op=='+'|| Op == '-'|| Op == '*'|| Op == '/'|| Op == '%')
+            //{
+            //    Console.WriteLine("Enter the 2 numbers in order number1 'Operation' number2");
+            //    int.TryParse(Console.ReadLine(), out int Num1);
+            //    int.TryParse(Console.ReadLine(), out int Num2);
+            //    switch (Op)
+            //    {
+            //        case '+':
+            //            Console.WriteLine(Num1 + Num2);
+            //            break;
+            //        case '-':
+            //            Console.WriteLine(Num1 - Num2);
+            //            break;
+            //        case '*':
+            //            Console.WriteLine(Num1 * Num2);
+            //            break;
+            //        case '/':
+            //            Console.WriteLine(Num1 / Num2);
+            //            break;
+            //        case '%':
+            //            Console.WriteLine(Num1 % Num2);
+            //            break;
+
+
+
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This operation is not supported");
+            //}
+
+
+
+            #endregion
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
